3a0bc69ccaa8488753a251493fc472c1d53a58f9
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-04 14:36:03 +0200

    Add download_packages.bat

---
+++ download_packages.bat
@@ -0,0 +1,8 @@
+
+set DIR=packages
+
+svn co https://github.com/smplayer-dev/smplayer.git/trunk %DIR%\smplayer
+svn co https://github.com/smplayer-dev/smplayer-themes.git/trunk %DIR%\smplayer-themes
+svn co https://github.com/smplayer-dev/smplayer-skins.git/trunk %DIR%\smplayer-skins
+
+svn co https://github.com/smplayer-dev/smtube.git/trunk %DIR%\smtube
