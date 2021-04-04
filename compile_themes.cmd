ec0f591b2bb513a1869032673ea31f4463076d65
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-04 15:06:31 +0200

    Add compile_themes.cmd

---
+++ compile_themes.cmd
@@ -0,0 +1,16 @@
+rem @echo off
+
+call version.cmd
+
+set WD=%cd%
+set ROOT=packages
+
+cd %ROOT%\smplayer-themes
+cd themes
+mingw32-make
+cd %WD%
+
+cd %ROOT%\smplayer-skins
+cd themes
+mingw32-make
+cd %WD%
