43a86bc4254473f91192a205d66dae5595eaeaa0
Ricardo <10128951+smplayer-dev@users.noreply.github.com>
2021-04-07 08:55:39 +0200

    Add create_portable_package.cmd

---
+++ create_portable_package.cmd
@@ -0,0 +1,25 @@
+set ROOT=packages
+set BUILD_DIR=%ROOT%\BUILD
+set WD=%cd%
+
+cd %ROOT%\smplayer-portable\
+call %WD%\getversion.cmd
+cd %WD%
+
+set PORTABLE_PATH=%BUILD_DIR%\smplayer-portable-build
+
+mkdir "%PORTABLE_PATH%\screenshots"
+echo [%%General]> "%PORTABLE_PATH%\smplayer.ini"
+echo screenshot_directory=.\\screenshots>> "%PORTABLE_PATH%\smplayer.ini"
+echo.>> "%PORTABLE_PATH%\smplayer.ini"
+echo [smplayer]>> "%PORTABLE_PATH%\smplayer.ini"
+echo check_if_upgraded=false>> "%PORTABLE_PATH%\smplayer.ini"
+copy "%PORTABLE_PATH%\smplayer.ini" "%PORTABLE_PATH%\smplayer_orig.ini"
+
+set PACKAGEFILENAME=smplayer-portable-%VER_MAJOR%.%VER_MINOR%.%VER_BUILD%.%VER_REVISION%.7z
+
+cd %PORTABLE_PATH%
+%WD%\7za a -t7z "..\output\%PACKAGEFILENAME%" "*" -mx9
+cd %WD%
+
+echo %PACKAGEFILENAME%
